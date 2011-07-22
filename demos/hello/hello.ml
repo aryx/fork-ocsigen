cb955506fe3248534347ca3b3b2c82869dd54c14
pad <pad@fb.com>
2011-07-22 14:38:05 -0700

    misc

--- demos/hello/hello.ml
+++ demos/hello/hello.ml
@@ -1,5 +1,5 @@
-(* this open is done by eliomc, but if you want to compile without eliomc,
- * then you need this. Otherwise you'll get a type error. Ugly ...
+(* this open is done also internally by eliomc, but if you want to compile
+ * without eliomc then you need it (otherwise you'll get a type error, ugly)
  *)
 open Eliom_pervasives
 
@@ -11,8 +11,8 @@ let coucou =
     (fun () () ->
       Lwt.return
         (HTML5.M.html
-          (HTML5.M.head (HTML5.M.title (HTML5.M.pcdata "")) [])
-          (HTML5.M.body [HTML5.M.h1 [HTML5.M.pcdata "How you doin?"]])))
+            (HTML5.M.head (HTML5.M.title (HTML5.M.pcdata "")) [])
+            (HTML5.M.body [HTML5.M.h1 [HTML5.M.pcdata "How you doin?"]])))
 
 (* with some open and a counter *)
 open HTML5.M
