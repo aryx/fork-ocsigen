5e22e19f971c433fcc3f647bcd09694f88f148b3
pad <pad@fb.com>
2011-08-01 12:06:47 +0200

    demos/graffity: better makefile

--- demos/graffiti/server.ml
+++ demos/graffiti/server.ml
@@ -12,33 +12,34 @@ let bus =
   Eliom_bus.create ~scope:`Global ~name:"grib" ~size:500 
     Json.t<Shared.messages>
 
-let draw_server, image_string = 
-  let surface = 
-    Cairo.image_surface_create Cairo.FORMAT_ARGB32 
-      ~width:Shared.width ~height:Shared.height in
+let surface = 
+  Cairo.image_surface_create Cairo.FORMAT_ARGB32 
+    ~width:Shared.width ~height:Shared.height
+
+let image_string () =
+  let b = Buffer.create 10000 in
+  (* Output a PNG in a string *)
+  Cairo_png.surface_write_to_stream surface (Buffer.add_string b);
+  Buffer.contents b
+  
+let draw_server = fun ((color : string), size, (x1, y1), (x2, y2)) ->
+
   let ctx = Cairo.create surface in
-  ((fun ((color : string), size, (x1, y1), (x2, y2)) ->
 
-    (* Set thickness of brush *)
-    Cairo.set_line_width ctx (float size) ;
-    Cairo.set_line_join ctx Cairo.LINE_JOIN_ROUND ;
-    Cairo.set_line_cap ctx Cairo.LINE_CAP_ROUND ;
-    let red, green, blue =  Lib.rgb_from_string color in
-    Cairo.set_source_rgb ctx ~red ~green ~blue ;
-
-    Cairo.move_to ctx (float x1) (float y1) ;
-    Cairo.line_to ctx (float x2) (float y2) ;
-    Cairo.close_path ctx ;
-    
-    (* Apply the ink *)
-    Cairo.stroke ctx ;
-   ),
-   (fun () ->
-     let b = Buffer.create 10000 in
-     (* Output a PNG in a string *)
-     Cairo_png.surface_write_to_stream surface (Buffer.add_string b);
-     Buffer.contents b
-   ))
+  (* Set thickness of brush *)
+  Cairo.set_line_width ctx (float size);
+  Cairo.set_line_join ctx Cairo.LINE_JOIN_ROUND;
+  Cairo.set_line_cap ctx Cairo.LINE_CAP_ROUND;
+  let red, green, blue =  Lib.rgb_from_string color in
+  Cairo.set_source_rgb ctx ~red ~green ~blue;
+  
+  Cairo.move_to ctx (float x1) (float y1);
+  Cairo.line_to ctx (float x2) (float y2);
+  Cairo.close_path ctx;
+  
+  (* Apply the ink *)
+  Cairo.stroke ctx;
+  ()
 
 let _ = Lwt_stream.iter draw_server (Eliom_bus.stream bus)
 
